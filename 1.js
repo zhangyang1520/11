cf898a5ba3f064ecc3c73702800f454207c4c336
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-19 18:52:11 +0800

    Update 1.js

--- 1.js
+++ 1.js
@@ -14,6 +14,6 @@ var body = $response.body;
 body = JSON.parse(body);
 body.data.is_vip = true;
 body.data.vip_time = "2999-09-09到期";
-body.data.nick_name = "by~伟人";
+body.data.nick_name = "by~恶魔";
 body.data.avatar_url = "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg";
 $done({body: JSON.stringify(body)});
