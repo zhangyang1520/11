a061849d18e71db2afefb6f58f835a5ccd549dfc
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-19 19:29:36 +0800

    Update and rename 1.js to 2.js

---
+++ 2.js
@@ -0,0 +1,31 @@
+
+/***************************************
+
+[mitm]
+
+hostname = *keyboard*
+
+[rewrite_local]
+
+
+^http[s]?:\/\/api\.keyboard\.buzhedie\.com\/input\/member_info\/.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/2.js
+
+
+
+***************************************/
+
+var body = $response.body;
+let obj = JSON.parse($response.body);
+obj={
+  "status": 1,
+  "msg": "success",
+  "error_code": 0,
+  "data": {
+    "is_vip": true,
+    "vip_time": "9999-09-09到期",
+    "nick_name": "by~恶魔",
+    "avatar_url": "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg"
+  }
+}
+
+$done({body: JSON.stringify(obj)});
