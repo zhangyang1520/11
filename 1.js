ed8ea0fedcca476337c392b5e8c3a1a392f88588
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-19 19:22:33 +0800

    1.js

--- 1.js
+++ 1.js
@@ -35,8 +35,8 @@ obj={
   "error_code": 0,
   "data": {
     "is_vip": true,
-    "vip_time": "2999-09-09到期",
-    "nick_name": "by~伟人",
+    "vip_time": "9999-09-09到期",
+    "nick_name": "by~恶魔",
     "avatar_url": "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg"
   }
 }
