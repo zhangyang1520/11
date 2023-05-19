22fe92f267ecfbb98fb83acd15418ec9d7551454
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-19 19:18:37 +0800

    1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,19 @@
+
+/***************************************
+
+脚本功能：不折叠输入法VIP
+下载地址：appstore
+软件版本：
+脚本作者：恶魔
+更新时间：2023-03-05
+问题反馈：316331369
+作者QQ:316331369
+QQ会员群：添加作者
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+忠告:请添加频道获取食用方法
+
+**************************************
+
 [mitm]
 
 hostname = *keyboard*
@@ -5,7 +21,7 @@ hostname = *keyboard*
 [rewrite_local]
 
 
-^http[s]?:\/\/api\.keyboard\.buzhedie\.com\/input\/member_info\/.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/1.js
+^http[s]?:\/\/api\.keyboard\.buzhedie\.com\/input\/member_info\/.*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/buzd.js
 
 
 
@@ -20,7 +36,9 @@ obj={
   "data": {
     "is_vip": true,
     "vip_time": "2999-09-09到期",
-    "nick_name": "by~恶魔",
+    "nick_name": "by~伟人",
     "avatar_url": "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg"
   }
 }
+
+$done({body: JSON.stringify(obj)});
