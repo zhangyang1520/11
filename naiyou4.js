04bab883c86d37c77b6ea0fc0c79ef7ba88f49e4
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-20 11:33:34 +0800

    Update naiyou4.js

--- naiyou4.js
+++ naiyou4.js
@@ -2,17 +2,38 @@
 [mitm]
 
 hostname = *naiyou*
-
 [rewrite_local]
-
-
-^http[s]?:\/\/nz-api\.duitang\.com\/account\/me\/?statics\=1 url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/naiyou4.js
-
+^http[s]?:\/\/nz-api\.duitang\.com\/account\/me\/\?statics\=1 .*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/naiyou4.js
 
 
 ***************************************/
+
 var body = $response.body;
-body = JSON.parse(body);
-body.data.vip=true;
-body.data.statics.payTotal=28;
-$done({body: JSON.stringify(body)});
+let obj = JSON.parse($response.body);
+obj={
+  "status" : 1,
+  "message" : "success",
+  "data" : {
+    "id" : 3825742,
+    "bindQQ" : false,
+    "vip" : true,
+    "version" : 1,
+    "bindWechat" : true,
+    "createAt" : "2023-05-19 23:39:48",
+    "avatar" : "",
+    "bindApple" : false,
+    "updateAt" : "2023-05-19 23:39:48",
+    "statics" : {
+      "payTotal" : 28,
+      "blogCollectTotal" : 0,
+      "blogDownloadTotal" : 0
+    },
+    "nickname" : "",
+    "telephone" : "2",
+    "status" : 0,
+    "bindPhone" : false
+  }
+}
+
+
+$done({body: JSON.stringify(obj)});
