e51a5a16dc36b7586f07438ea860c2cf716caff1
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-26 11:24:43 +0800

    Create 4.js

---
+++ 4.js
@@ -0,0 +1,43 @@
+/**************************************
+
+[mitm]
+
+hostname = *naiyou3*
+
+[rewrite_local]
+
+
+
+^http[s]?:\/\/nz-api\.duitang\.com\/account\/me\/\?statics\=1.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/3.js
+
+***************************************/
+
+var body = $response.body;
+let obj = JSON.parse($response.body);
+obj={
+    "status" : 1,
+    "message" : "success",
+    "data" : {
+      "id" : 9999999,
+      "bindQQ" : true,
+      "vip" : true,
+      "version" : 1,
+      "bindWechat" : true,
+      "createAt" : "2023-05-19 23:39:48",
+      "avatar" : "",
+      "bindApple" : false,
+      "updateAt" : "2023-05-19 23:39:48",
+      "statics" : {
+        "payTotal" : 28,
+        "blogCollectTotal" : 0,
+        "blogDownloadTotal" : 0
+      },
+      "nickname" : "恶魔",
+      "telephone" : "3",
+      "status" : 1,
+      "bindPhone" : false
+    }
+  }
+  
+
+$done({body: JSON.stringify(obj)});
