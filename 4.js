29ed1af1b005e7ea0445bd0295dbebeaa08fcdc5
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-26 11:35:51 +0800

    Update 4.js

--- 4.js
+++ 4.js
@@ -8,7 +8,7 @@ hostname = *naiyou3*
 
 
 
-^http[s]?:\/\/nz-api\.duitang\.com\/account\/me\/\?statics\=1.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/3.js
+^http[s]?:\/\/nz-api\.duitang\.com\/account\/me\/\?statics\=1.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/4.js
 
 ***************************************/
 
