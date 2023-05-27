9a1aa25fd8670bc852b87c8791072e4d8d5f3325
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-28 00:46:06 +0800

    Create xiongmao.js

---
+++ xiongmao.js
@@ -0,0 +1,33 @@
+/**************************************
+
+[mitm]
+
+hostname = *anduril.xmkanshu.com*
+
+[rewrite_local]
+
+^ http[s]?:\/\/anduril\.xmkanshu.com\/v1\/91user\/ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xiongmao.js
+
+***************************************/
+
+var body = $response.body;
+
+var url = $request.url;
+
+var obj = JSON.parse(body);
+
+const vip = '/91user/';
+
+if (url.indexOf(vip) != -1)
+
+{
+
+  obj. isVip="'1";
+
+obj. UserVipLevel=3;
+
+obj. Vip10=1;
+
+obj. Vip10="2099-06-30";
+
+body = JSON.stringify(obj);}$done({body});
