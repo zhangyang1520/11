181f2b3aeb1215e55677a27d0adb55d27a85ad2b
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-29 11:36:46 +0800

    Update xm_sleep.js

--- xm_sleep.js
+++ xm_sleep.js
@@ -3,7 +3,7 @@
 hostname=*sleep.navolove.com*
 
 [rewrite_local]
-^http[s]?:\/\/sleep.navolove.com\/api\/refresh_user_info
+^http[s]?:\/\/sleep.navolove.com\/api\/refresh_user_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
 
 *********************************************************/
 
