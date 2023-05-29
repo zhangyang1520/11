b0e600f180b438c5208429c7d69c36547576a387
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-29 11:52:59 +0800

    Update xm_sleep.js

--- xm_sleep.js
+++ xm_sleep.js
@@ -3,13 +3,13 @@
 hostname=*sleep.navolove.com*
 
 [rewrite_local]
-^http[s]?:\/\/sleep.navolove.com\/api\/refresh_user_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
+^http[s]?:\/\/sleep.navolove.com\/api\/refresh\_user\_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
 *********************************************************/
 
 var body = $response.body;
 var url = $response.url;
 var obj = JSON.parse(body);
-const api_url = /refresh_user_info?/;
+const api_url = /refresh_user_info?;
 if(url.indexOf(api_url) != -1) {
   obj.data.nickname = "by-恶魔";
   obj.data.is_svip = true;
