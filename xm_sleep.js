cb4d4e97283af4af5b99039016cd1b1ffdd61f02
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-29 20:23:24 +0800

    Update xm_sleep.js

--- xm_sleep.js
+++ xm_sleep.js
@@ -1,10 +1,27 @@
-/*********************************************************
-[mitm]
-hostname=*sleep.navolove.com*
+/*******************************
+
+脚本功能：熊猫睡眠——解锁VIP
+软件下载：https://is.gd/nTgaTp
+软件版本：v1.1.3
+脚本作者：彭于晏
+更新时间：2023-5-25
+问题反馈：QQ+89996462
+QQ会员群：779392027💞
+TG反馈群：https://t.me/plus8889
+TG频道群：https://t.me/py996
+使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
+
+*******************************
 
 [rewrite_local]
-^http[s]?:\/\/sleep.navolove.com\/api\/refresh\_user\_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
-*********************************************************/
+
+^http[s]?:\/\/sleep.navolove.com\/api\/refresh_user_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
+
+[mitm] 
+
+hostname = sleep.navolove.com
+
+*******************************/
 
 var body = $response.body;
 var url = $response.url;
