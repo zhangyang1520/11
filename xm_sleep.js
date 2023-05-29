75b52dcab54abdb6a8866745052801be28bb7ef8
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-29 20:10:55 +0800

    Update xm_sleep.js

--- xm_sleep.js
+++ xm_sleep.js
@@ -6,8 +6,6 @@ hostname=*sleep.navolove.com*
 ^http[s]?:\/\/sleep.navolove.com\/api\/refresh\_user\_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
 *********************************************************/
 
-^http[s]?:\/\/sportlight.wayxtech.com\/miniapp\/user\/getUserInfo
-
 var body = $response.body;
 var url = $response.url;
 var obj = JSON.parse(body);
