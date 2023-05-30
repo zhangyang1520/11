de5e0f464f7d2c64975a0e9a1e8cdabc7a90fe05
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-05-30 13:23:30 +0800

    Update mixiaoya.js

--- mixiaoya.js
+++ mixiaoya.js
@@ -16,13 +16,13 @@ const vip2 = 'buyRechargeCode?';
 if (url.indexOf(vip) != -1) {
   obj.body. balance = 15;
 }
-if (url.indexOf(vip1) != -1) {
+if (url.indexOf(vip2) != -1) {
   obj. success = true;
-    obj. data. preNum = 10;
-   obj. data. resText = "充值成功";
-   obj. data. dedcPre = 10;
-   obj. data. balance = 5;
-   obj. msg = "充值成功";
+  obj. data. preNum = 10;
+  obj. data. resText = "充值成功";
+  obj. data. dedcPre = 10;
+  obj. data. balance = 5;
+  obj. msg = "充值成功";
 }
 
 body = JSON.stringify(obj);
