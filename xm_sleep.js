a2605d2430fda3448f4e50a2596c3432ed86f3ee
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-05 15:22:15 +0800

    Update xm_sleep.js

--- xm_sleep.js
+++ xm_sleep.js
@@ -1,21 +1,16 @@
 /*******************************
 
 脚本功能：熊猫睡眠——解锁VIP
-软件下载：https://is.gd/nTgaTp
 软件版本：v1.1.3
-脚本作者：彭于晏
+脚本作者：恶魔
 更新时间：2023-5-25
-问题反馈：QQ+89996462
-QQ会员群：779392027💞
-TG反馈群：https://t.me/plus8889
-TG频道群：https://t.me/py996
 使用声明：⚠️此脚本仅供学习与交流，请勿转载与贩卖！⚠️⚠️⚠️
 
 *******************************
 
 [rewrite_local]
 
-^http[s]?:\/\/sleep.navolove.com\/api\/refresh_user_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
+^http[s]?:\/\/sleep.navolove.com\/api\/refresh\_user\_info url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/xm_sleep.js
 
 [mitm] 
 
@@ -28,10 +23,13 @@ var url = $response.url;
 var obj = JSON.parse(body);
 const api_url = '/refresh_user_info?';
 if(url.indexOf(api_url) != -1) {
-  obj.data.nickname = "by-恶魔";
-  obj.data.is_svip = true;
-  obj.is_vip = true;
-  obj.expiration_time = "2099-09-29";
+obj=JSON.parse(jsonStr);
+obj.data.nickname="by_恶魔破解";
+obj.data.is_svip=true;
+obj.data.is_vip=true;
+obj.data.expiration='2099-09-09';
+obj.data.expiration_time='2099-09-09';
+obj.data.username="by_恶魔破解";
 }
 body = JSON.stringify(obj);
 $done({body: body});
