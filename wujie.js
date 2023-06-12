6835ce9338f23a3e48afac3ed38cfd45a08da404
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-12 08:24:30 +0800

    Update wujie.js

--- wujie.js
+++ wujie.js
@@ -12,12 +12,12 @@ const list1=["copyrightUserInfo","userMemberCardList","buyMemberCardList"];
 for (let i of list1) {
     if(i in obj){
         if(i===list1[0]){
-            obj.copyrightUserInfo.availableBalance =9999;
-            obj. copyrightUserInfo.nickname="by_恶魔破解";
-            obj. copyrightUserInfo.userRoleInfo.aiArtist=true;
-            obj. copyrightUserInfo.userRoleInfo.artist=true;
-            obj. copyrightUserInfo.memberDetail.endTime= 4102041723;
-            obj. copyrightUserInfo.memberDetail.hold= true;
+            obj.copyrightUserInfo.availableBalance=9999;
+            obj.copyrightUserInfo.nickname="by_恶魔破解";
+            obj.copyrightUserInfo.userRoleInfo.aiArtist=true;
+            obj.copyrightUserInfo.userRoleInfo.artist=true;
+            obj.copyrightUserInfo.memberDetail.endTime=4102041723;
+            obj.copyrightUserInfo.memberDetail.hold=true;
         } else if(i===list1[1]){
             obj={
                 "accountModule" : {
