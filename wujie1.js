4bed173ede8153d6e0754192b45fb624e7047331
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-13 22:56:37 +0800

    Update wujie1.js

--- wujie1.js
+++ wujie1.js
@@ -7,10 +7,10 @@ hostname = *ate.wujiebantu.com*
 ^http[s]?:\/\/gate\.wujiebantu\.com\/one\-graph\-auth\/graphql\/ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/wujie1.js
 
 ***************************************/
-let obj=$response.body;
-body = JSON.parse(body);
+let body=$response.body;
+obj = JSON.parse(body);
 const list1=["copyrightUserInfo","userMemberCardList","buyMemberCardList"];
-if (list1[0] in body){
+if (list1[0] in obj){
     obj.data.copyrightUserInfo.integralWalletBalance.availableBalance=9999;
     obj.data.copyrightUserInfo.nickname="by_恶魔破解";
     obj.data.copyrightUserInfo.userRoleInfo.aiArtist=true;
@@ -18,4 +18,4 @@ if (list1[0] in body){
     obj.data.copyrightUserInfo.memberDetail.endTime=4102041723;
     obj.data.copyrightUserInfo.memberDetail.hold=true;
 };
-$done({body: JSON.stringify(body)});
+$done({body: JSON.stringify(obj)});
