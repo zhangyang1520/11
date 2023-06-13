a73a23c43f80905a2705630b00adfb02d2d38e22
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-14 01:44:34 +0800

    Update wujie1.js

--- wujie1.js
+++ wujie1.js
@@ -7,9 +7,11 @@ hostname = *gate.wujiebantu.com*
 ^http[s]?:\/\/gate\.wujiebantu\.com\/one\-graph\-auth\/graphql\/ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/wujie1.js
 
 ***************************************/
-var body=$response.body;
+var timestamp = new Date().valueOf() / 1000;
+var resbody=$response.body;
+var body = $request.body;
 var obj = JSON.parse(body);
 if ("nickname" in obj){
     obj.data.copyrightUserInfo.nickname="by_恶魔破解"
-}
+}$done({header: {"time": Math.floor(timestamp)}});
 $done({body: JSON.stringify(obj)});
