0fa07d44747c9b7f64e2cb7afd71049ef23810cd
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-14 00:15:36 +0800

    Update wujie1.js

--- wujie1.js
+++ wujie1.js
@@ -9,7 +9,7 @@ hostname = *gate.wujiebantu.com*
 ***************************************/
 var body=$response.body;
 var obj = JSON.parse(body);
-if (copyrightUserInfo in obj){
+if (nickname in obj){
     obj.data.copyrightUserInfo.nickname="by_恶魔破解"
 }
 $done({body: JSON.stringify(obj)});
