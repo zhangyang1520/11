24ceae7fa3b3243b1816212f56560316ad435361
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-15 11:04:29 +0800

    Create qupai.js

---
+++ qupai.js
@@ -0,0 +1,14 @@
+/***************************************
+[mitm]
+hostname = *s.mcwifi.cn*
+
+[rewrite_local]
+^http[s]?:\/\/s.mcwifi.cn\/hw/apple\/subscribe\/v1.*$ url script-response-body https://raw.githubusercontent.com/zhangyang1520/11/main/qupai.js
+
+***************************************/
+
+var body = $response.body;
+body = JSON.parse(body);
+body.data.isVip = "1";
+body.data.expiresDateMs = "1";
+$done({body: JSON.stringify(body)});
