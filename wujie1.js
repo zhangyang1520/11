5d8a60bd9256b0e6d8acfe52ece17d35c197a6b6
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-15 21:54:37 +0800

    Update wujie1.js

--- wujie1.js
+++ wujie1.js
@@ -13,7 +13,12 @@ var body = $response.body;
 var obj = JSON.parse(body);
 
 if(obj.data.hasOwnProperty(c)){
-    obj.data[c].nickname="EMO";
+    obj.data[c].availableBalance=9999;
+    obj.data[c].nickname="by_恶魔破解";
+    obj.data[c].userRoleInfo.aiArtist=true;
+    obj.data[c].userRoleInfo.artist=true;
+    obj.data[c].memberDetail.endTime=4102041723;
+    obj.data[c].memberDetail.hold=true;
 }
 
 $done({body: JSON.stringify(obj)});
