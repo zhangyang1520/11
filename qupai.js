c2898b57ec55826afa415f800c535a1c3cc61d2c
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-15 12:59:56 +0800

    Update qupai.js

--- qupai.js
+++ qupai.js
@@ -10,5 +10,5 @@ hostname = *s.mcwifi.cn*
 var body = $response.body;
 body = JSON.parse(body);
 body.data.isVip = "1";
-body.data.expiresDateMs = "9999999";
+body.data.expiresDateMs = "32495443200000";
 $done({body: JSON.stringify(body)});
