567fea86257e739b3fe9f98db6095292f387f705
zhangyang1520 <134059622+zhangyang1520@users.noreply.github.com>
2023-06-16 20:19:09 +0800

    Update wujie2.js

--- wujie2.js
+++ wujie2.js
@@ -25,9 +25,8 @@ if(obj.data.hasOwnProperty(p)){
    obj.data[p].userMemberCardList[0].cardType="PLATINUM_CARD";
    obj.data[p].userMemberCardList[0].name="珀金权益卡";}
 
-if(obj.data[p].hasOwnProperty(L)){
+/*if(obj.data[p].hasOwnProperty(L)){
    obj.data[p][L][0].holdCardInfo[0].num=99;
-}
+}*/
 
-   
  $done({body: JSON.stringify(obj)});
